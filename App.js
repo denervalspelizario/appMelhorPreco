bfa7130422ef467a9637d19e3b25cfc69e0dc852
denervalspelizario <denervalspelizario@gmail.com>
2023-02-10 12:28:07 -0300

    update appMelhorPreco criacao e implementacao component tela inicial

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View,  } from 'react-native';  // 
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';  // 1 import Modal e um botão TouchableOpacity
+import TelaInicial from './component/TelaInicial'; // importando tela inicial que sera colcado como conteudo do modal inicial
 
+
+// 1 - tela inicial 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      modalVisible: true
+    }
+    this.telaInicial = this.telaInicial.bind(this); // 1 - bind para linkar  dados do state com a funcao telaInicial
+  };
+
+  telaInicial(){  // 1 - altera state modalvisible de true para false ao clicar no botão (ver linha 17 TelaInicial.js)
+    if(this.state.modalVisible == true){   
+      this.setState({modalVisible: false})  
+    };
+  };
+
+
+
   render(){
   return (
     <View style={styles.container}>
+      <Modal  // 1 - Modal Tela Inicial
+        transparent={false} // 1 - deixa o fundo fora do modal invisivel
+        animationType='fade' // 1 - modal vai sumir sem animação por isso o fade 
+        visible={this.state.modalVisible} // 1 - mostra quando sera MOSTRADA o modal, neste caso quando o state modalVisible estiver true 
+                                          // 1 - se inicia como true(ligado) pois sera a tela inicial 
+      >
+        {/* 1 - componente que armazena todas as infos da tela inicial(modal), recebe props(dados) que funciona ao clicar(onPress) leia linha 17 TelaInicial.js  */}
+        <TelaInicial  offTelaInicial={this.telaInicial} /> 
+      </Modal>
+
       
     </View>
   );
---
+++ component/TelaInicial.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View,Image, TouchableOpacity, } from 'react-native';  // 1 import  botão TouchableOpacity
+import BombaCombustivel from '../assets/combustivel.png'
+
+// 1 - tela inicial 
+class TelaInicial extends Component {
+
+  render(){
+  return (
+    <View style={styles.containerTelaInicial}>
+        <View style={styles.containerCombustivel}>
+            <Text style={styles.textCombustivel}>Melhor Preço</Text>
+            <Image
+                source={BombaCombustivel}
+                style={styles.imageCombustivel}
+            />
+            <TouchableOpacity style={styles.btnCombustivel} onPress={this.props.offTelaInicial}>
+                <Text style={styles.textBtn}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
+      
+    </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  containerTelaInicial: {
+    flex: 1, 
+    backgroundColor: '#8A2BE2',
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  },
+  textCombustivel: {
+    color: '#FFF',
+    fontSize: 25,
+    fontFamily: 'Roboto',
+
+  },
+  imageCombustivel: {
+    width: 150,
+    height: 150
+  },
+  containerCombustivel:  {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btnCombustivel: {
+    width: 100,
+    height: 50,
+    
+    
+  },
+  textBtn: {
+    fontSize: 15,
+    backgroundColor: '#FFF',
+    textAlign: 'center',
+    color: '#8a2be2',
+    padding: 10,
+    borderRadius: 20,
+  }
+  
+});
+
+
+export default TelaInicial;
